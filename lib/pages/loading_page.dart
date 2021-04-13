412ec15ba8ab50dca988bd57acd768f2af0786f8
Nicholas <nicoar9@gmail.com>
2021-04-13 17:05:49 -0500

    :sparkles:Feature: Initial configuration and fade transition on loading page

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:map_app/helpers/helpers.dart';
+import 'package:map_app/pages/mapa_page.dart';
+
+class LoadingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: checkGpsLocation(context),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          return Center(
+              child: CircularProgressIndicator(
+            strokeWidth: 2,
+          ));
+        },
+      ),
+    );
+  }
+}
+
+Future checkGpsLocation(BuildContext context) async {
+  await Future.delayed(Duration(milliseconds: 1000));
+
+  Navigator.pushReplacement(
+    context,
+    navegarMapaFadeIn(
+      context,
+      MapaPage(),
+    ),
+  );
+}
