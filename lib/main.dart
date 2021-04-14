b023c71fbba56ca103d07b2d2761e8fe88dc47c8
Nicholas <nicoar9@gmail.com>
2021-04-14 13:43:53 -0500

    :sparkles:Feature: Initial bloc config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:map_app/bloc/my_location/my_location_bloc.dart';
 import 'package:map_app/pages/gps_access_page.dart';
 import 'package:map_app/pages/loading_page.dart';
 import 'package:map_app/pages/map_page.dart';
@@ -8,15 +10,22 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      home: LoadingPage(),
-      routes: {
-        'map': (_) => MapPage(),
-        'loading': (_) => LoadingPage(),
-        'gps_access': (_) => GpsAccessPage()
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => MyLocationBloc(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
+        home: LoadingPage(),
+        routes: {
+          'map': (_) => MapPage(),
+          'loading': (_) => LoadingPage(),
+          'gps_access': (_) => GpsAccessPage()
+        },
+      ),
     );
   }
 }
